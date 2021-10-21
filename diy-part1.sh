6086f8a7c393828d2e78a5c4d37f272de1cd6d9a
YL2209 <59274897+YL2209@users.noreply.github.com>
2021-10-21 23:28:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/CHN-beta/xmurp-ua.git package/lean/xmurp-ua
+# 添加插件源码
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# passwall依赖
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
